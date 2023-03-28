ded5f8137582838a39d44797e5462802b872f5fa
Petrolias Christoforos <cpetrolias@evolutionProjects.gr>
2023-03-28 23:19:41 +0300

    initial commit, adding basic models

---
+++ QM.Models/InputModels/BaseRegistration.cs
@@ -0,0 +1,17 @@
+﻿namespace QM.Models.InputModels
+{
+    internal class BaseRegistration
+    {
+        public BaseRegistration()
+        {
+            this.CreatedAt = DateTime.UtcNow;
+        }
+
+        public BaseRegistration(DateTime createdAt)
+        {
+            this.CreatedAt = createdAt;
+        }
+
+        public DateTime CreatedAt { get; set; }        
+    }
+}
---
+++ QM.Models/InputModels/Registration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QM.Models.InputModels
+{
+    internal class Registration : BaseRegistration
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }        
+    }
+}
